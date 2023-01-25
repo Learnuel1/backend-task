8415be0219794e1addb19bcf384744655e1ea065
Learnuel1 <leanueltech@gmail.com>
2023-01-25 15:39:09 +0100

    fix: modified .en.js file

--- src/app.js
+++ src/app.js
@@ -3,16 +3,9 @@ const morgan = require("morgan");
 const cors = require('cors');
 const cookieparser=require('cookie-parser');
 const {urlencoded} = require('express');
-const { getFrontendOrigin } = require('./config/env');
 const CONFIG = require('./config');
-const ORIGIN = getFrontendOrigin();
 const app = express();
-app.use(cors({
-    origin:ORIGIN,
-    methods:['GET','PUT','POST','DELETE','PATCH'],
-    credentials:true
-}));
- app.set("trust proxy",1)
+
 app.use(morgan('dev'));
 app.use(cookieparser());
 app.disable('etag'); 
